594ee350ba865b84bc0fe4c31c6efa65c1207ecb
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2017-05-19 10:29:26 +0200

    Fixing cosmetic formatting in the licenses

--- src/gtk-boilerplate.c
+++ src/gtk-boilerplate.c
@@ -3,13 +3,13 @@
  *
  * This source file is part of Gtk-boilerplate.
  *
- * Gtk-boilerplate is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Gtk-boilerplate is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * Gtk-boilerplate is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * Gtk-boilerplate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
