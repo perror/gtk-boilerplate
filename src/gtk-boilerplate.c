a61efb3b2a673ec07e5e17cc46a991e2ed179e22
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2017-05-22 19:20:39 +0200

    Adding a menubar to the main window

--- src/gtk-boilerplate.c
+++ src/gtk-boilerplate.c
@@ -39,6 +39,13 @@ handle_local_options (GApplication __unused *app,
   return RETURN_FAILURE;
 }
 
+/* Call-back to quit the application */
+static void
+quit (GtkApplication __unused * app)
+{
+  g_print("Quit\n");
+}
+
 /* Call-back to setup application when run by the Desktop environment */
 static void
 activate (GApplication * app)
@@ -47,6 +54,38 @@ activate (GApplication * app)
   GtkWidget *window = gtk_application_window_new (GTK_APPLICATION (app));
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  /* Setting main box */
+  GtkWidget *main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  gtk_container_add(GTK_CONTAINER(window), main_box);
+
+  /* Adding a menubar to the main window */
+  GtkWidget *menubar = gtk_menu_bar_new();
+  gtk_box_pack_start(GTK_BOX(main_box), menubar, FALSE, FALSE, 0);
+
+  /* Adding a 'File' menu */
+  GtkWidget *file_menu = gtk_menu_new();
+  GtkWidget *file_menu_file = gtk_menu_item_new_with_label("File");
+  gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_menu_file), file_menu);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file_menu_file);
+
+  /* Adding a 'File->Open' submenu */
+  GtkWidget *file_menu_open = gtk_menu_item_new_with_label("Open");
+  gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), file_menu_open);
+
+  /* Adding a 'File->Preferences' submenu */
+  GtkWidget *file_menu_prefs = gtk_menu_item_new_with_label("Preferences");
+  gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), file_menu_prefs);
+
+  /* Adding a separator in the menu */
+  GtkWidget *file_menu_sep = gtk_separator_menu_item_new();
+  gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), file_menu_sep);
+
+  /* Adding a 'File->Quit' submenu */
+  GtkWidget *file_menu_quit = gtk_menu_item_new_with_label("Quit");
+  gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), file_menu_quit);
+
+  g_signal_connect (file_menu_quit, "activate", G_CALLBACK (quit), app);
 
   /* Display the window and its content */
   gtk_widget_show_all (window);
